d265ea5616640332a3b173d3c86735f07574272e
Maietskyi <damaietskyi@gmail.com>
2025-02-04 23:46:22 +0200

    lesson-1-6-metadata

---
+++ src/app/(public)/about/page.tsx
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+    return (
+        <div>
+            about page content
+        </div>
+    );
+};
+
+export default AboutPage;
--- src/app/(public)/users/[id]/layout.tsx
+++ src/app/(public)/users/[id]/layout.tsx
@@ -5,6 +5,7 @@ export const metadata: Metadata = {
     title: 'UserLayout metadata'
 }
 type Props = {children: React.ReactNode}
+
 const UserLayout = ({children}: Props) => {
     return (
         <>
