b762987b69c11e6396c4c941308972d15405cda1
Maietskyi <damaietskyi@gmail.com>
2025-02-05 22:17:58 +0200

    lesson-2/2-3-simple-form

--- src/services/api.service.tsx
+++
@@ -1,7 +0,0 @@
-import {IUser} from "@/models/IUser";
-
-export const getAllUsers = async ():Promise<IUser[]> => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json());
-    return users;
-}
