75c6bddb69e72421ea3bcbdebaf4ff756d2115cd
Maietskyi <damaietskyi@gmail.com>
2025-02-06 23:40:45 +0200

    lesson-2/2-4-server-actions-and-mutations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import Form from "next/form";
+import {getMeals, saveMeal} from "@/server-actions/server-actions";
 
-export default function Home() {
+export default async function Home() {
+
+    const mealsArray = await getMeals();
+
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            {/*<form action="/submit">*/}
-            {/*    <input type="text" name={"name"} placeholder={'login'}/>*/}
-            {/*    <input type="text" name={"password"} placeholder={'password'}/>*/}
-            {/*    <button>submit</button>*/}
-            {/*</form>*/}
 
-            <Form action={'/submit'}>
-                    <input type="text" name={"name"} placeholder={'login'}/>
-                    <input type="text" name={"password"} placeholder={'password'}/>
+            <Form action={saveMeal}>
+                    <input type="text" name={"title"} placeholder={'text'}/>
+                    {/*<input type="text" name={"password"} placeholder={'password'}/>*/}
                     <button>submit</button>
             </Form>
+
+            <div className={'meals'}>
+                {mealsArray.map(meal => (<div key={meal.id}>{meal.title}</div>))}
+            </div>
         </div>
     );
 }
