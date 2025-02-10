6cf17538687c8aadfdaa5ab9684371fb1de29f13
Maietskyi <damaietskyi@gmail.com>
2025-02-10 12:30:05 +0200

    lesson-3 / 1-SSG

---
+++ src/app/users/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const UsersPage = async () => {
+
+    const users = await fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json());
+
+    return (
+        <div>
+            Users Page
+            {users.map((user: {id:number, username:string}) => (<div key={user.id}>
+                {user.username}
+            </div>))}
+        </div>
+    );
+};
+
+export default UsersPage;
+
+// Це підхід до сторінок на яких інформація рідко оновлюється
